52ff037450df4f90f0f978c83d8a0379b00e8708
beauthebeau <beau@beauthebeau.pro>
2023-12-16 00:04:17 +0000

    refactor: Remove cooldown for `/ping`

--- src/commands/miscCommands/ping.js
+++ src/commands/miscCommands/ping.js
@@ -5,11 +5,6 @@ module.exports = {
     .setName("ping")
     .setDescription("Retrieves the bot's ping"),
 
-  options: {
-    permissions: { ownerOnly: false, devOnly: false },
-    cooldown: { enabled: true, time: 5 },
-  },
-
   async execute(interaction) {
     await interaction.reply(
       `🏓 Pong! **Websocket**: ${interaction.client.ws.ping}ms, **REST**: ${
